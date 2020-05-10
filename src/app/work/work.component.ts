fa70c32d8f478b12efe6f53ea903bd7884a99e24
Renny Mittal <rennymittal1234@gmail.com>
2020-05-10 17:35:17 +0530

    connected backend

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import AOS from 'aos';
+import { HttpClient } from '@angular/common/http';
+import { Route, Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-work',
@@ -8,14 +11,49 @@ import AOS from 'aos';
 })
 export class WorkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient,
 
+    private router: Router,
+    private formBuilder: FormBuilder) { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  name;
+  email;
+  message;
+  recieve;
   ngOnInit() {
     AOS.init({
     })
-   
-  }
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+  });
+}
 
+get f() { return this.registerForm.controls; }
+    url = "https://rishabh-dev-portfolio.herokuapp.com/mail"
+
+    onSubmit() {
+      this.submitted = true;
+      if (this.registerForm.invalid) {
+          return;
+      }    
+      else
+      {
+        let json={
+          name:this.name,
+          message: this.message,
+          email : this.email,
+          recieve : 'rennymittal1214@gmail.com'
+        };
+          this.httpClient.post(this.url,json).subscribe((res: any)=>{
+              alert("mail sent"+this.name);
+          }
+          );
+      } 
+  }
   projects = [
     {
       title: "Little Oak",
